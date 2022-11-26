ae457a6cd971c0a5002a7e34e7c31f64595cd775
z16166 <zhangbo@gmail.com>
2022-11-26 15:56:03 +0800

    按边长递增的顺序打印结果

--- CountSquare.cpp
+++ CountSquare.cpp
@@ -1,24 +1,41 @@
 #include <algorithm>
 #include <assert.h>
 #include <format>
+#include <map>
 #include <set>
 #include <string>
 #include <vector>
+
+struct RectangleInfo {
+  int lenShort;
+  int lenLong;
+};
 
 static constexpr int N = 10;
 static constexpr int e[N] = {3, 3, 3, 3, 4, 4, 7, 7, 7, 7}; // 'e' means edge
 static int used[N];
 static int len[5] = {};
+
 static std::set<std::string> solutions;
 
-inline bool IsValid() {
-  return len[1] == len[3] && len[2] == len[4] && len[1] && len[2];
-}
+auto comp = [](const RectangleInfo &a, const RectangleInfo &b) {
+  if (a.lenShort < b.lenShort)
+    return true;
+  else if (a.lenShort > b.lenShort)
+    return false;
+  else {
+    if (a.lenLong < b.lenLong)
+      return true;
+    else
+      return false;
+  }
+};
 
-inline void Append(const std::vector<int> &a,
-                   std::vector<std::vector<int>> &r) {
-  r.emplace_back(a);
-}
+static std::map<RectangleInfo, std::set<std::string>, decltype(comp)> solutions2;
+
+inline bool IsValid() { return len[1] == len[3] && len[2] == len[4] && len[1] && len[2]; }
+
+inline void Append(const std::vector<int> &a, std::vector<std::vector<int>> &r) { r.emplace_back(a); }
 
 inline int Compare(const std::vector<int> &a, const std::vector<int> &b) {
   if (a[0] < b[0]) {
@@ -48,8 +65,7 @@ inline int Compare(const std::vector<int> &a, const std::vector<int> &b) {
   }
 }
 
-inline void Merge(const std::vector<int> &a, const std::vector<int> &b,
-                  std::vector<std::vector<int>> &r) {
+inline void Merge(const std::vector<int> &a, const std::vector<int> &b, std::vector<std::vector<int>> &r) {
   const auto cmp = Compare(a, b);
   switch (cmp) {
   case -1:
@@ -90,7 +106,7 @@ inline bool IsDuplicate() {
     if (size1 >= size2) {
       Merge(s[1], s[3], r);
       Merge(s[2], s[4], r);
-    } else if (size1 < size2) {
+    } else {
       Merge(s[2], s[4], r);
       Merge(s[1], s[3], r);
     }
@@ -107,8 +123,20 @@ inline bool IsDuplicate() {
   }
 
   if (std::get<1>(solutions.insert(h))) {
-    printf("solution %zu (sorted edge numbers: %s):\n", solutions.size(),
-           h.c_str());
+    //printf("solution %zu (sorted edge numbers: %s):\n", solutions.size(), h.c_str());
+
+    RectangleInfo info;
+    info.lenShort = (std::min)(len[1], len[2]);
+    info.lenLong = (std::max)(len[1], len[2]);
+
+    std::set<std::string> t;
+    t.emplace(h);
+
+    auto pair = solutions2.insert(std::pair<RectangleInfo, std::set<std::string>>(info, t));
+    if (!std::get<1>(pair)) {
+      std::get<0>(pair)->second.emplace(h);
+    }
+
     return false;
   }
 
@@ -141,8 +169,9 @@ void DFS(int k) {
     len[state] += e[k];
 
     if (k == (N - 1)) {
-      if (IsValid() && !IsDuplicate())
-        Print();
+      if (IsValid() && !IsDuplicate()) {
+        //Print();
+      }
     } else {
       DFS(k + 1);
     }
@@ -152,10 +181,17 @@ void DFS(int k) {
 }
 
 int main() {
-  DFS(0);
+  DFS(0);  
+
+  size_t k = 0;
+  for (const auto &i : solutions2) {
+    printf("%d x %d\n", i.first.lenShort, i.first.lenLong);
+    for (const auto &h : i.second) {
+      printf("\tsorted solution %zu: %s\n", ++k, h.c_str());
+    }
+  }
+  
   printf("total solutions: %zu\n", solutions.size());
-  for (const auto &i : solutions)
-    printf("%s\n", i.c_str());
 
   return 0;
 }
